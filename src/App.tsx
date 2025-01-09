72e5d9de0083c1fc73c8a31d39c6a8a4445029f9
Victor Varela <victor398@gmail.com>
2025-01-09 13:06:10 -0300

    install tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-
-import './App.css'
-
 function App() {
-  
-
   return (
     <>
-      <h1>calorie tracker</h1>
+      <h1 className="text-3xl font-bold underline">calorie tracker</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
