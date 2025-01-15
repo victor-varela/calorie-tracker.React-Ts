d6c79ae54f047e87a2e8a77cf814f300ff397d4e
Victor Varela <victor398@gmail.com>
2025-01-15 12:06:55 -0300

    feat: add formData State

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,16 @@ function App() {
         <h1 className="font-bold text-white uppercase text-xl">Contador de Calorias</h1>
         <button>Reiniciar App</button>
       </header>
-      <Form />
+      <section className="bg-lime-500 py-20">
+      {/* contendor "centrador-brocha gorda" que rodea al Componente */}
+        <div className="max-w-4xl mx-auto"> 
+          <Form />
+        </div>
+      </section>
     </>
   );
 }
 
 export default App;
+
+//Los contenedores de presentacion "brocha gorda" se dejan en App.tsx y los estilos de los componentes se hacen en el mismo componente. Este enfoque dicta que si voy a renderizar un componente que esta dentro de un contenedor entonces ese contenedor estilizarlo fuera del componente. Asi el componente es mas "limpio" con los estilos mas personalizados. El div que rodea a Form es un div "centrador- para ubicarlo en el centro del documento solo establece las medidas" el Form tiene en sus estilos la apariencia mas especifica. Entonces, usa los Divs centradores o de maquetacion como en este caso el section solo tiene el color de fondo y se aplica a todo el ancho del documento y la separacion vertical y el div que rodea al Form que ya se dijo su funcion. Se puede decir que hay 2 divs por cada componente, el "grande- Section" y el "centrador" luego viene el componente.
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,46 +1,79 @@
+import { useState } from "react";
 import { categories } from "../data/data";
+
+
 const Form = () => {
+
+  const [formData, setFormData] = useState({
+    category: 1,
+    activity: "",
+    calories: 0,
+  });
+
+  const handelChange = () => {
+    console.log("changed..");
+  };
+
   return (
-    <div className="bg-lime-500 py-20">
-      <form className="max-w-4xl bg-white mx-auto border rounded-lg shadow p-8 space-y-3">
-        <div className="flex flex-col gap-3">
-          <label htmlFor="category" className="font-bold" id="categories">
-            Categoria:
-          </label>
-          <select className="border rounded-lg border-slate-300 bg-white w-full p-2" name="categories" id="category">
-            {categories.map(category => (
-              <option key={category.id} value={category.id}>{category.name}</option>
-            ))}
-          </select>
-        </div>
-        <div className="flex flex-col gap-3">
-          <label htmlFor="activity" className="font-bold">Actividad:</label>
-          <input
-            type="text"
-            className="border rounded-lg border-slate-300 bg-white w-full p-2 "
-            name="activity"
-            id="activity"
-            placeholder="Ej. Pesas, Yoga, Trote"
-          />
-        </div>
-        <div className="flex flex-col gap-3">
-          <label htmlFor="calories" className="font-bold">Calorias:</label>
-          <input
-            type="number"
-            className="border rounded-lg border-slate-300 bg-white w-full p-2 pl-2"
-            name="calories"
-            id="calories"
-            placeholder="Ej. 200, 300"
-          />
-        </div>
-        <input 
-          type="submit"
-          value="Guardar Comida o Guardar ejercicio"
-          className="w-full bg-gray-800 hover:bg-gray-900 font-bold text-white uppercase rounded p-2 cursor-pointer"
+    <form className="max-w-4xl bg-white mx-auto border rounded-lg shadow p-8 space-y-3">
+      <div className="flex flex-col gap-3">
+        <label htmlFor="category" className="font-bold" id="categories">
+          Categoria:
+        </label>
+        <select 
+          className="border rounded-lg border-slate-300 bg-white w-full p-2" 
+          id="category" 
+          value={formData.category}
+          onChange={()=>handelChange()}
+        >
+          {categories.map(category => (
+            <option key={category.id} value={category.id} >
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="flex flex-col gap-3">
+        <label htmlFor="name" className="font-bold">
+          Actividad:
+        </label>
+        <input
+          type="text"
+          className="border rounded-lg border-slate-300 bg-white w-full p-2 "
+          id="name"
+          placeholder="Ej. Pesas, Yoga, Trote"
+          value={formData.activity}
+          onChange={()=>handelChange()}
         />
-      </form>
-    </div>
+      </div>
+      <div className="flex flex-col gap-3">
+        <label htmlFor="calories" className="font-bold">
+          Calorias:
+        </label>
+        <input
+          type="number"
+          className="border rounded-lg border-slate-300 bg-white w-full p-2 pl-2"
+          id="calories"
+          placeholder="Ej. 200, 300"
+          value={formData.calories}
+          onChange={()=>handelChange()}
+        />
+      </div>
+      <input
+        type="submit"
+        value="Guardar Comida o Guardar ejercicio"
+        className="w-full bg-gray-800 hover:bg-gray-900 font-bold text-white uppercase rounded p-2 cursor-pointer"
+      />
+    </form>
   );
 };
 
 export default Form;
+
+// Este componente Form retorna un Form element---> esta bien, es mas coherente.
+// Repaso de Html. Form, los lables deben tener el htmlFor atributte con el mismo nombre que el Id del input para que esten asociados
+// Vieja leccion de FORM.. los input deben tener el htmlfor=id === atributo id del input. Y name igual para todos y value DIFERENTE. Los valores de un FORM se leen con e => e.target.value y por medio de onChange. Otra leccion: para que ts tome el type de value se agrega sigo + antes de e porque por default los value son strings y lo estamos declarando como number. OTRA VEZ!!
+
+// OJO: segun la forma de trabajo del profesor me doy cuenta que los estilos de "ubicacion" ej: width, height, margin auto, flex, los declara en el contenedor de presentacion. Digamos en los "contenedores de brocha gorda" los que solo ubican el componente en el documento y los estilos "finos, de brocha fina" ej: bg-color, text, border, shadow, rounden los declara en el Componente mismo.
+
+//El State: el state de este componente por ser un formulario conviene que sea un OBJETO. si, Js, Ts es todo un OBJETO! por que conviene por ser un formulario? porque los formularios sus campos estan RELACIONADOS entonces toda la data de un formulario es un OBJETO.  Como llamar a este STATE? veamos, es un formulario que guarda 2 cosas (categorias) actividad o comida. Se puede llamar Formdata (nombre bien generico), activityEntry, inputValues, etc.. las nombres de las propiedades de ese objeto son los nombres de los campos del formulario tal cual estan en sus labels
