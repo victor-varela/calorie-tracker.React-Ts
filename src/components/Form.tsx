73df839a451ae2ee1c62be9175b02da54da9bee7
Victor Varela <victor398@gmail.com>
2025-01-16 11:48:03 -0300

    feat: save data in State

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,17 +1,18 @@
 import { useState } from "react";
 import { categories } from "../data/data";
 
-
 const Form = () => {
-
   const [formData, setFormData] = useState({
     category: 1,
-    activity: "",
+    name: "",
     calories: 0,
   });
 
-  const handelChange = () => {
-    console.log("changed..");
+  const handelChange = e => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
   };
 
   return (
@@ -20,14 +21,14 @@ const Form = () => {
         <label htmlFor="category" className="font-bold" id="categories">
           Categoria:
         </label>
-        <select 
-          className="border rounded-lg border-slate-300 bg-white w-full p-2" 
-          id="category" 
+        <select
+          className="border rounded-lg border-slate-300 bg-white w-full p-2"
+          id="category"
           value={formData.category}
-          onChange={()=>handelChange()}
+          onChange={handelChange}
         >
           {categories.map(category => (
-            <option key={category.id} value={category.id} >
+            <option key={category.id} value={category.id}>
               {category.name}
             </option>
           ))}
@@ -43,7 +44,7 @@ const Form = () => {
           id="name"
           placeholder="Ej. Pesas, Yoga, Trote"
           value={formData.activity}
-          onChange={()=>handelChange()}
+          onChange={handelChange}
         />
       </div>
       <div className="flex flex-col gap-3">
@@ -56,7 +57,7 @@ const Form = () => {
           id="calories"
           placeholder="Ej. 200, 300"
           value={formData.calories}
-          onChange={()=>handelChange()}
+          onChange={handelChange}
         />
       </div>
       <input
@@ -76,4 +77,8 @@ export default Form;
 
 // OJO: segun la forma de trabajo del profesor me doy cuenta que los estilos de "ubicacion" ej: width, height, margin auto, flex, los declara en el contenedor de presentacion. Digamos en los "contenedores de brocha gorda" los que solo ubican el componente en el documento y los estilos "finos, de brocha fina" ej: bg-color, text, border, shadow, rounden los declara en el Componente mismo.
 
-//El State: el state de este componente por ser un formulario conviene que sea un OBJETO. si, Js, Ts es todo un OBJETO! por que conviene por ser un formulario? porque los formularios sus campos estan RELACIONADOS entonces toda la data de un formulario es un OBJETO.  Como llamar a este STATE? veamos, es un formulario que guarda 2 cosas (categorias) actividad o comida. Se puede llamar Formdata (nombre bien generico), activityEntry, inputValues, etc.. las nombres de las propiedades de ese objeto son los nombres de los campos del formulario tal cual estan en sus labels
+//El State: el state de este componente por ser un formulario conviene que sea un OBJETO. si, Js, Ts es todo un OBJETO! por que conviene por ser un formulario? porque los formularios sus campos estan RELACIONADOS entonces toda la data de un formulario es un OBJETO.  Como llamar a este STATE? veamos, es un formulario que guarda 2 cosas (categorias) actividad o comida. Se puede llamar Formdata (nombre bien generico), activityEntry, inputValues, etc.. las nombres de las propiedades de ese objeto son los nombres de los campos del formulario tal cual estan en sus labels-
+
+//Para sincronizar el state con el formulario debemos tener el VALUE (es el que va a tener el valor de ese input) Y ONCHANGE
+
+//La tecnica de la ABUELA para sincronizar inputs a un state OBJETO: los Ids de cada input deben ser IGUALES a las KEYS del objeto State. Se usa la sintaxis de corchetes y se crea una copia del objeto antes de la actualizacion.
