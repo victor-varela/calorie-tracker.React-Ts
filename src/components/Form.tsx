8455c3e0e0397e4c65de9ba7eb9819b8e8ecba15
Victor Varela <victor398@gmail.com>
2025-01-21 11:49:27 -0300

    feat: Add type check for form fields to ensure data integrity during updates"

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -10,11 +10,11 @@ const Form = () => {
 
   //Los type de e lo sacamos de Vs Code y usamos un pipe | para indicar que puede ser o tipo SelectElement o InputElement
   const handelChange = (e: React.ChangeEvent<HTMLSelectElement> | React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e);
+    const isNumberField = ["category", "calories"].includes(e.target.id);
 
     setFormData({
       ...formData,
-      [e.target.id]: e.target.value,
+      [e.target.id]: isNumberField ? +e.target.value : e.target.value,
     });
   };
 
@@ -76,12 +76,14 @@ export default Form;
 
 // Este componente Form retorna un Form element---> esta bien, es mas coherente.
 // Repaso de Html. Form, los lables deben tener el htmlFor atributte con el mismo nombre que el Id del input para que esten asociados
-// Vieja leccion de FORM.. los input deben tener el htmlfor=id === atributo id del input. Y name igual para todos y value DIFERENTE. Los valores de un FORM se leen con e => e.target.value y por medio de onChange. Otra leccion: para que ts tome el type de value se agrega sigo + antes de e porque por default los value son strings y lo estamos declarando como number. OTRA VEZ!!
+// Vieja leccion de FORM.. los input deben tener el htmlfor=id === atributo id del input. Y name igual para todos y value DIFERENTE. Los valores de un FORM se leen con e => e.target.value y por medio de onChange. Otra leccion: para que ts tome el type de value se agrega signo + antes de e porque por default los value son strings y lo estamos declarando como number. OTRA VEZ!!
 
-// OJO: segun la forma de trabajo del profesor me doy cuenta que los estilos de "ubicacion" ej: width, height, margin auto, flex, los declara en el contenedor de presentacion. Digamos en los "contenedores de brocha gorda" los que solo ubican el componente en el documento y los estilos "finos, de brocha fina" ej: bg-color, text, border, shadow, rounden los declara en el Componente mismo.
+// OJO: segun la forma de trabajo del profesor me doy cuenta que los estilos de "ubicacion" ej: width, height, margin auto, flex, los declara en el contenedor de presentacion. Digamos en los "contenedores de brocha gorda" los que solo ubican el componente en el documento y los estilos "finos, de brocha fina" ej: bg-color, text, border, shadow, rounded los declara en el Componente mismo.
 
-//El State: el state de este componente por ser un formulario conviene que sea un OBJETO. si, Js, Ts es todo un OBJETO! por que conviene por ser un formulario? porque los formularios sus campos estan RELACIONADOS entonces toda la data de un formulario es un OBJETO.  Como llamar a este STATE? veamos, es un formulario que guarda 2 cosas (categorias) actividad o comida. Se puede llamar Formdata (nombre bien generico), activityEntry, inputValues, etc.. las nombres de las propiedades de ese objeto son los nombres de los campos del formulario tal cual estan en sus labels-
+//El State: el state de este componente por ser un formulario conviene que sea un OBJETO. si, Js, Ts es todo un OBJETO! por que conviene por ser un formulario? porque en los formularios sus campos estan RELACIONADOS entonces toda la data de un formulario es un OBJETO.  Como nombrar a este STATE? veamos, es un formulario que guarda 2 cosas (categorias) actividad o comida. Se puede llamar Formdata (nombre bien generico), activityEntry, inputValues, etc.. las nombres de las propiedades de ese objeto son los nombres de los campos del formulario tal cual estan en sus labels-
 
 //Para sincronizar el state con el formulario debemos tener el VALUE (es el que va a tener el valor de ese input) Y ONCHANGE
 
-//La tecnica de la ABUELA para sincronizar inputs a un state OBJETO: los Ids de cada input deben ser IGUALES a las KEYS del objeto State. Se usa la sintaxis de corchetes y se crea una copia del objeto antes de la actualizacion.
+//La tecnica de la ABUELA para sincronizar inputs a un state OBJETO: los nombres de los Ids de cada input deben ser IGUALES a las KEYS del objeto State. Se usa la sintaxis de corchetes y se crea una copia del objeto antes de la actualizacion.
+
+//Tenemos en nuestro State propiedades con diferentes tipos de dato. category y calories son number y activity es string. Tenemos que respetar la integridad de esos tipos y en un Form por ser Form, los campos se guardan como string. Ya lo vimos en vieja leccion de Form mas arriba. Entonces, ANTES de guardar los campos en el state tenemos que: 1- Identificar si el campo donde esta el usuario es de tipo number. Si es asi, entonces el dato lo guardamos con la tecnica de signo de + antes de la instruccion. Si no esta en el campo number entonces se guarda asi nomas. En el codigo eso es lo que hace la funcion isNumberField --> nos dice si esta en ese campo el usuario. Como sabemos? por el id del input.Piensa en un arreglo como una lista y en .includes(valor) como preguntar:"¿Esta lista tiene este objeto específico?"
